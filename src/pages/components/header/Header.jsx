f1385b8f65b8b101620465b77d7e734d92dc6e1c
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-08-11 23:20:30 +0300

    Added basic Navbar

--- src/pages/components/header/Header.jsx
+++ src/pages/components/header/Header.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import "./Header.css";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="navbar">
+      <div className="logo">
+        <img src="https://via.placeholder.com/50x50" alt="Logo" />
+        <span>Data Bar</span>
+      </div>
+      <nav className="nav-links">
+        <a href="#">Pricing</a>
+        <a href="#">Dashboard</a>
+        <a href="#">Profile</a>
+      </nav>
+      <div className="auth-buttons">
+        <a href="#" className="login">
+          Login
+        </a>
+        <a href="#" className="signup">
+          Sign Up
+        </a>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
