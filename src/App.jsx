0c4d3a84b8051ab263dcd57fa7fbb36232872f37
AliM-1997 <aliimortada797@gmail.com>
2024-08-12 11:48:31 +0300

    tested the animated graph

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { Provider } from "react-redux";
 import store from "../src/data/redux/store";
 import Dashboard from "./pages/dashboard/Dashboard";
 import Signup from "./pages/signup/Signup";
+import Graph from "./pages/graph/Graph";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
             <Route path="login" element={<Login />} />
             <Route path="signup" element={<Signup />} />
             <Route path="dashboard" element={<Dashboard />} />
+            <Route path="graph" element={<Graph />} />
           </Route>
         </Routes>
       </Router>
