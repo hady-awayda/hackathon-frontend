4422a5e1d2826fc0d3920386fd3f5b9a7787aeec
Alirh1 <alirhayem.1212@gmail.com>
2024-08-12 04:42:22 +0300

    added navigation in header

--- src/pages/components/header/Header.jsx
+++ src/pages/components/header/Header.jsx
@@ -1,24 +1,49 @@
 import React from "react";
 import "./Header.css";
 import logo from "../../../assets/logo.png";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const navigate = useNavigate();
+  const handlePricing = () =>{
+    navigate("/pricing");
+  }
+  
+  const handleDashboard = () =>{
+    navigate("/dashboard");
+  }
+
+  const handlepProfile = () =>{
+    navigate("/profile");
+  }
+
+  const handleLogin = () =>{
+    navigate("/login");
+  }
+
+  const handleSignup = () =>{
+    navigate("/signup");
+  }
+
+  const handlehome = () =>{
+    navigate("/home");
+  }
   return (
     <header className="navbar">
       <div className="logo">
         <img src={logo} alt="Logo" />
-        <span>Data Bar</span>
+        <span onClick={handlehome} className="databar">Data Bar</span>
       </div>
       <nav className="nav-links">
-        <a href="#">Pricing</a>
-        <a href="#">Dashboard</a>
-        <a href="#">Profile</a>
+        <a onClick={handlePricing}>Pricing</a>
+        <a onClick={handleDashboard}>Dashboard</a>
+        <a onClick={handlepProfile}>Profile</a>
       </nav>
       <div className="auth-buttons">
-        <a href="#" className="login">
+        <a onClick={handleLogin} className="login">
           Login
         </a>
-        <a href="#" className="signup">
+        <a onClick={handleSignup} className="signup">
           Sign Up
         </a>
       </div>
